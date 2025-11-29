f563e276439d4d1d218f9259853de00c6e477176
Hank100194 <hank100194@tvu-onschool.edu.vn>
2025-11-29 10:40:10 +0700

    initial project

---
+++ WebBanHangOnline/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Owin.Security.Provider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/Contact
+        public ActionResult Index()
+        {
+            var items = db.Contact;
+            return View(items);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                //model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.Contact.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            var item = db.Contact.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                //model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.Contact.Attach(model);
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+      
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.Contact.Find(id);
+            if (item != null)
+            {
+                db.Contact.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+
+            return Json(new { success = false });
+        }
+
+
+
+    }
+}
---
+++ WebBanHangOnline/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+namespace WebBanHangOnline.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var items = db.Contact;
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                //model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.Contact.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
